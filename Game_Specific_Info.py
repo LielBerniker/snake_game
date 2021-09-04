de068abac9d9820d9f8bcbaee64e219484c254c4
liel berniker <lielb101995@gmail.com>
2021-09-04 10:30:56 +0300

    opp extend

--- Game_Specific_Info.py
+++ Game_Specific_Info.py
@@ -13,7 +13,6 @@ class Game_Specific_Info:
         self.x1_change = 0
         self.y1_change = 0
         self.count_time = timeit.default_timer()
-        self.current_time = 0
         self.snake_List = []
         self.Length_of_snake = 1
         self.rand_num = 0
@@ -29,14 +28,14 @@ class Game_Specific_Info:
         return round(random.randrange(0, dis_height - snake_b) / 10.0) * 10.0
 
     def collect_toy(self, ggi: Game_General_Info):
-        if (self.toy_x <= self.x1 <= self.toyx + float(ggi.toy_block)) and (
+        if (self.toy_x <= self.x1 <= self.toy_x + float(ggi.toy_block)) and (
                 self.toy_y <= self.y1 <= self.toy_y + float(ggi.toy_block)):
             return True
         elif (self.x1 <= self.toy_x <= self.x1 + float(ggi.snake_block)) and (
                 self.toy_y <= self.y1 <= self.toy_y + float(ggi.toy_block)):
             return True
         elif (self.x1 <= self.toy_x <= self.x1 + float(ggi.snake_block)) and (
-                self.y1 <= self.toy_y <= self.y + float(ggi.snake_block)):
+                self.y1 <= self.toy_y <= self.y1 + float(ggi.snake_block)):
             return True
         elif (self.toy_x <= self.x1 <= self.toy_x + float(ggi.toy_block)) and (
                 self.y1 <= self.toy_y <= self.y1 + float(ggi.snake_block)):
@@ -49,3 +48,25 @@ class Game_Specific_Info:
             self.toy_y = self.round_position_y(ggi.dis_height, ggi.snake_block)
             self.Length_of_snake += 1
             self.rand_num = random.randrange(len(ggi.toys_container))
+
+    def check_in_screen(self, ggi: Game_General_Info):
+        if self.x1 >= ggi.dis_width or self.x1 < 0 or self.y1 >= ggi.dis_height or self.y1 < 0:
+            self.game_close = True
+
+    def update_snake(self):
+        self.x1 += self.x1_change
+        self.y1 += self.y1_change
+        snake_Head = [self.x1, self.y1]
+        self.snake_List.append(snake_Head)
+        if len(self.snake_List) > self.Length_of_snake:
+            del self.snake_List[0]
+
+        for x in self.snake_List[:-1]:
+            if x == snake_Head:
+                self.game_close = True
+
+    def change_state(self):
+        current_time = timeit.default_timer()
+        if current_time - self.count_time > 0.4:
+            self.state = self.state * (-1)
+            self.count_time = current_time
--- main.py
+++ main.py
@@ -2,18 +2,13 @@ import pygame
 import time
 import random
 import timeit
+from Game_General_Info import Game_General_Info
+from Game_Specific_Info import Game_Specific_Info
 
 if __name__ == '__main__':
 
     pygame.init()
-
-    white = (255, 255, 255)
-    black = (0, 0, 0)
-    red = (213, 50, 80)
-    pink = (255, 131, 250)
-
-    dis_width = 800
-    dis_height = 600
+    ggi = Game_General_Info(600, 800, 20, 40, 12)
     bg = pygame.image.load("photos/room.jpeg")
     face_0 = pygame.image.load("photos/face_0.PNG")
     face_1 = pygame.image.load("photos/face_1.PNG")
@@ -21,26 +16,19 @@ if __name__ == '__main__':
     horse = pygame.image.load("photos/horse.PNG")
     ball = pygame.image.load("photos/pool_ball.PNG")
     wood = pygame.image.load("photos/wood.PNG")
-    toys_container = []
-    toys_container.append(circle)
-    toys_container.append(horse)
-    toys_container.append(wood)
-    toys_container.append(ball)
+    ggi.set_toys(circle, horse, ball, wood)
 
-    screen = pygame.display.set_mode((dis_width, dis_height))
+    screen = pygame.display.set_mode((ggi.dis_width, ggi.dis_height))
     pygame.display.set_caption('Snake Game new style')
 
     clock = pygame.time.Clock()
-    toy_block = 20
-    snake_block = 40
-    snake_speed = 12
 
     font_style = pygame.font.SysFont("bahnschrift", 25)
     score_font = pygame.font.SysFont("comicsansms", 35)
 
 
     def Your_score(score):
-        value = score_font.render("Your Score: " + str(score), True, white)
+        value = score_font.render("Your Score: " + str(score), True, ggi.white)
         screen.blit(value, [0, 0])
 
 
@@ -51,106 +39,72 @@ if __name__ == '__main__':
             pic = face_1
         len_list = len(snake_list)
         for x in range(len_list - 1):
-            pygame.draw.rect(screen, pink, [snake_list[x][0], snake_list[x][1], snake_block, snake_block])
+            pygame.draw.rect(screen, ggi.pink, [snake_list[x][0], snake_list[x][1], snake_block, snake_block])
 
         screen.blit(pic, (snake_list[-1][0], snake_list[-1][1]))
 
 
     def message(msg, color):
         mesg = font_style.render(msg, True, color)
-        screen.blit(mesg, [dis_width / 6, dis_height / 3])
+        screen.blit(mesg, [ggi.dis_width / 6, ggi.dis_height / 3])
+
 
     def draw_toy(toyx, toyy, rand_num):
-        screen.blit(toys_container[rand_num], (toyx, toyy))
+        screen.blit(ggi.toys_container[rand_num], (toyx, toyy))
 
 
-    def gameLoop():
-        game_over = False
-        game_close = False
-        state = -1
-        x1 = dis_width / 2
-        y1 = dis_height / 2
+    def gameLoop(ggi: Game_General_Info):
+        gsi = Game_Specific_Info(ggi)
 
-        x1_change = 0
-        y1_change = 0
-        count_time = timeit.default_timer()
-        snake_List = []
-        Length_of_snake = 1
-        rand_num = 0
+        while not gsi.game_over:
 
-        toy_x = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-        toy_y = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-
-        while not game_over:
-
-            while game_close:
-                screen.fill(black)
+            while gsi.game_close:
+                screen.fill(ggi.black)
                 screen.blit(bg, (0, 0))
-                message("You Lost! Press C-Play Again or Q-Quit", red)
-                Your_score(Length_of_snake - 1)
+                message("You Lost! Press C-Play Again or Q-Quit", ggi.red)
+                Your_score(gsi.Length_of_snake - 1)
                 pygame.display.update()
 
                 for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_q:
-                            game_over = True
-                            game_close = False
+                            gsi.game_over = True
+                            gsi.game_close = False
                         if event.key == pygame.K_c:
                             gameLoop()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    game_over = True
+                    gsi.game_over = True
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
-                        x1_change = -toy_block
-                        y1_change = 0
+                        gsi.x1_change = -ggi.toy_block
+                        gsi.y1_change = 0
                     elif event.key == pygame.K_RIGHT:
-                        x1_change = toy_block
-                        y1_change = 0
+                        gsi.x1_change = ggi.toy_block
+                        gsi.y1_change = 0
                     elif event.key == pygame.K_UP:
-                        y1_change = -toy_block
-                        x1_change = 0
+                        gsi.y1_change = -ggi.toy_block
+                        gsi.x1_change = 0
                     elif event.key == pygame.K_DOWN:
-                        y1_change = toy_block
-                        x1_change = 0
+                        gsi.y1_change = ggi.toy_block
+                        gsi.x1_change = 0
 
-            if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-                game_close = True
-            x1 += x1_change
-            y1 += y1_change
-            screen.fill(black)
+            gsi.check_in_screen(ggi)
+            gsi.update_snake()
+            screen.fill(ggi.black)
             screen.blit(bg, (0, 0))
-            draw_toy(toy_x, toy_y, rand_num)
-            snake_Head = []
-            snake_Head.append(x1)
-            snake_Head.append(y1)
-            snake_List.append(snake_Head)
-            if len(snake_List) > Length_of_snake:
-                del snake_List[0]
+            draw_toy(gsi.toy_x, gsi.toy_y, gsi.rand_num)
 
-            for x in snake_List[:-1]:
-                if x == snake_Head:
-                    game_close = True
-
-            our_snake(snake_block, snake_List, state)
-            Your_score(Length_of_snake - 1)
+            our_snake(ggi.snake_block, gsi.snake_List, gsi.state)
+            Your_score(gsi.Length_of_snake - 1)
             pygame.display.update()
-            current_time = timeit.default_timer()
-            if current_time - count_time > 0.4:
-                state = state * (-1)
-                count_time = current_time
-
-            if collect_toy(x1, y1, toy_x, toy_y):
-                toy_x = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-                toy_y = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-                Length_of_snake += 1
-                rand_num = random.randrange(len(toys_container))
-
-            clock.tick(snake_speed)
+            gsi.change_state()
+            gsi.toy_pos(ggi)
+            clock.tick(ggi.snake_speed)
 
         pygame.quit()
         quit()
 
 
-    gameLoop()
+    gameLoop(ggi)
